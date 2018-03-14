83342ac8f26554fc38612e0dde67e00dad427ecc
Jadson Santos <jadson@info.ufrn.br>
2018-03-14 16:22:59 -0300

    infraestrutura de uma arquitetura microservices
    Signed-off-by: Jadson Santos <jadson@info.ufrn.br>

---
+++ CoreMicroservicesLayer/src/main/java/com/example/CoreMicroservicesLayer/CoreMicroservicesLayerApplication.java
@@ -0,0 +1,12 @@
+package com.example.CoreMicroservicesLayer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoreMicroservicesLayerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoreMicroservicesLayerApplication.class, args);
+	}
+}
